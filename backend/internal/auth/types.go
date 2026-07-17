425c662f5c6b3624f4c491d9577a337789bcf760
victorouma7 <oumaasoyoh@gmail.com>
2026-07-17 22:01:18 +0300

    all untracked file

---
+++ backend/internal/auth/types.go
@@ -0,0 +1,57 @@
+package auth
+//user details and information
+type Role string
+
+const (
+    RoleOwner            Role = "owner"
+    RoleArchitect        Role = "architect"
+    RoleQuantitySurveyor Role = "quantity_surveyor"
+    RoleEngineer         Role = "engineer"
+    RoleContractor       Role = "contractor"
+    RoleSupplier         Role = "supplier"
+)
+type User struct {
+	Fullname 		string
+	ID	   			string
+	PasswordHash	string
+	Email 			string
+	Role		   	Role
+ 	IsVerified     	bool
+}
+// request and response for user registration, login, and verification
+type RegisterRequest struct {
+	Fullname 		string `json:"fullname"`
+ 	Password 		string `json:"password"`
+	Email 			string `json:"email"`
+	Role    		Role	`json:"role"`
+}
+type LoginRequest struct {
+ 	Email 			string `json:"email"`
+	Password 		string `json:"password"`
+}
+type VerifyRequest struct {
+	Token 			string `json:"token"`
+	Email 			string `json:"email"`
+}
+type LoginResponse struct {
+	Token 			string `json:"token"`
+	Email 			string `json:"email"`
+	Role            Role	`json:"role"`
+}
+type VerifyResponse struct {
+	Token 			string `json:"token"`
+	Email 			string `json:"email"`
+	Role            Role	 `json:"role"`
+}
+type RegisterResponse struct {
+	Token 			string `json:"token"`
+	Email 			string `json:"email"`
+	Role            Role `json:"role"`
+}type ErrorResponse struct {
+	Message 		string `json:"message"`
+}
+type JWTClaims struct {
+	ID				string `json:"id"`
+ 	Role            Role `json:"role"`
+}
+exp
